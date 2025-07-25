83d5147c75a09c7e8a7a4416e3ccb3ca52d57ad7
charlie <charliehu2808@outlook.com>
2025-07-25 03:23:01 +0200

    we can get blastoise now

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/MemoryGame.css'
 import {Card} from './Card.jsx'
 
@@ -7,8 +7,15 @@ for (let i = 1; i< 11; i++) {
         initCards.push({id: i, guessed: false});
     }
 
+
 export function MemoryGame () {
-    
+    const [imageURL, setImageURL] = useState(null)
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon/9", {mode: "cors"})
+            .then((response) => response.json())
+            .then(((response) => setImageURL(response["sprites"]["other"]["official-artwork"]["front_default"])))
+    }, [])
+
     const [cards, setCards] = useState(initCards)
     const [bestScore, setBestScore] = useState(0)
 
@@ -53,6 +60,7 @@ export function MemoryGame () {
         />
     ))}
     </div>
+    <img src={imageURL} alt={"Image did not fetch"}/>
     </>
     )
 }
