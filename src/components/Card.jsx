ff45fe647ba3e9fff0ae9349b3c8350d8ece2b1d
charlie <charliehu2808@outlook.com>
2025-07-25 02:26:04 +0200

    Reset upon wrong guess, best score implemented

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import '../styles/Card.css'
 export function Card ({id, guessed, handleCardChange}) {
     return (
         <>     
-            <div onClick={() =>handleCardChange({id: id, guessed: true})} className='card'>{id}, {guessed ? <h1>guessed</h1> : <>notGuessed</>}</div>
+            <div onClick={() => handleCardChange({id: id, guessed: guessed})} className='card'>{id}, {guessed ? <>guessed</> : <>notGuessed</>}</div>
         </>
     )
 }
--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -10,22 +10,43 @@ for (let i = 1; i< 11; i++) {
 export function MemoryGame () {
     
     const [cards, setCards] = useState(initCards)
+    const [bestScore, setBestScore] = useState(0)
+
     const guessedCount = cards.filter(card => card.guessed).length;
 
-    const handleCardChange = (nextCard) => {
-        setCards(
-            cards.map(card => {
-                return card.id == nextCard.id ? nextCard : card;
-            })
-        )
+    const shuffleList = (array) => {
+        array.sort(() => Math.random() - 0.5);
+    };
+    shuffleList(cards)
+
+    const handleCardChange = ({id, guessed}) => {
+        if (guessed) {
+            if (bestScore < guessedCount) {
+                setBestScore(guessedCount)
+            }
+            setCards(
+                cards.map(card => {
+                    return {...card, guessed: false}
+                    }
+                )
+            )
+        } else {
+            setCards(
+                cards.map(card => {
+                    return card.id == id ? {id: id, guessed: true} : card;
+                   }
+                )
+            )
+        }
     }
 
     return (
     <> 
-    <h2>Counter: {guessedCount}</h2>
+    <h2>Counter: {guessedCount}, Best Score: {bestScore}</h2>
     <div className='card-holder'>
     {cards.map(card => (
         <Card 
+        key={card.id}
         id={card.id}
         guessed={card.guessed}
         handleCardChange={handleCardChange}
