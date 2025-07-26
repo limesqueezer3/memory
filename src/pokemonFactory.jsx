9bb2323e04bb9f5e4d71a9fc1aeaecf2bffbcf86
charlie <charliehu2808@outlook.com>
2025-07-26 23:05:19 +0200

    little steps

--- src/pokemonFactory.jsx
+++ src/pokemonFactory.jsx
@@ -1,10 +1,10 @@
 export function pokemonFactory() {
     const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-    const getPokemon = (entry) => {
-        
-        fetch(`${baseUrl}${entry}`, {mode: "cors"})
-            .then((response) => response.json())
-            .then(((response) => (response["sprites"]["other"]["official-artwork"]["front_default"])))
+    const getPokemonArt = async (entry) => {
+        const response = await fetch(`${baseUrl}${entry}`, {mode: "cors"})
+        const responseJson = await response.json()
+        const artURL = responseJson["sprites"]["other"]["official-artwork"]["front_default"]
+        return artURL
     } 
 }
